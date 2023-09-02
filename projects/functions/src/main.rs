a037c2ab5fce46ba26487741921f0f59b14f2968
anantvijaysingh <anantvijay.23@gmail.com>
2023-09-02 23:01:05 +0200

    Day 9

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,16 +1,23 @@
 fn main() {
     println!("Hello, world!");
 
-    another_funtion(5);
+    another_function(5);
 
     print_labeled_measurements(5, '👌');
+
+    println!("The square of 7 is {}", square(7));
+
 }
 
-fn another_funtion(x: i32) {
+fn another_function(x: i32) {
     println!("Another function!");
     println!("The value of x is {}", x);
 }
 
-fn print_labeled_measurements(value: i32, unit_lable: char) {
-    println!("The measurement value is {}{}", value, unit_lable)
+fn print_labeled_measurements(value: i32, unit_label: char) {
+    println!("The measurement value is {}{}", value, unit_label)
+}
+
+fn square (x: i32) -> i32{
+    i32::pow(x, 2)
 }
