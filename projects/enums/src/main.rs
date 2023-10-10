0bc37b40094e90230a66f43f29bc362204471072
anantvijaysingh <anantvijay.23@gmail.com>
2023-10-10 22:52:51 +0200

    Day 22

--- projects/enums/src/main.rs
+++ projects/enums/src/main.rs
@@ -12,6 +12,21 @@ impl Message {
     }
 }
 
+#[derive(Debug)]
+enum USState {
+    California,
+    Washington,
+    // -- snip --
+}
+
+enum Coin {
+    Penny,
+    Nickle,
+    Dime,
+    Quarter(USState),
+}
+
+
 fn main() {
     let m_1 = Message::Write(String::from("Hello"));
     m_1.call();
@@ -19,4 +34,33 @@ fn main() {
     let m_2 = Message::Quit;
     m_2.call();
 
+
+    // Option
+
+    let some_number = Some(5);
+    let some_word: Option<String> = Some(String::from("Hey"));
+
+    let absent_number: Option<i32> = None;
+
+    // match
+    let penny = Coin::Penny;
+    value_in_cents(penny);
+
+    value_in_cents(Coin::Quarter(USState::California));
+
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky Penny");
+            1
+        },
+        Coin::Nickle => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}", state);
+            25
+        },
+    }
 }
