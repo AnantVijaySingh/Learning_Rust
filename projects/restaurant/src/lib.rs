16763536f927969379addbb8a4ee22dbf9056514
anantvijaysingh <anantvijay.23@gmail.com>
2023-10-22 21:42:34 +0200

    Day 25

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -3,30 +3,87 @@ pub fn add(left: usize, right: usize) -> usize {
 }
 
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {
-
+    pub mod hosting {
+        pub fn add_to_waitlist() {
         }
 
         fn seat_at_table () {
-
         }
+
     }
 
     mod serving {
         fn take_order() {
-
         }
 
         fn serve_order () {
-
         }
 
         fn take_payment () {
-
         }
     }
 }
+
+mod back_of_house {
+
+    #[derive(Debug)]
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer (toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("Peaches"),
+            }
+        }
+
+        pub fn food_order_confirmation (meal: &Breakfast) {
+            println!("{:?}", meal);
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() { }
+}
+
+fn deliver_order () {
+}
+
+pub fn eat_at_restaurant () {
+
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // In case of Structs, fields are private by default even for a public Struct
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+
+    back_of_house::Breakfast::food_order_confirmation(&meal);
+
+    // In the case of enum, all files are public for a public enum
+    let order_1 = back_of_house::Appetizer::Soup;
+    let order_2 = back_of_house::Appetizer::Salad;
+
+}
+
+
 
 #[cfg(test)]
 mod tests {
