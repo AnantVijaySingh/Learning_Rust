3ec935948ad4768f51546507035b93a0e63686ac
anantvijaysingh <anantvijay.23@gmail.com>
2023-11-05 22:33:47 +0100

    Day 31

--- projects/hash_maps/src/main.rs
+++ projects/hash_maps/src/main.rs
@@ -12,8 +12,31 @@ fn main() {
 
     println!("{} has {} points", house_name, houses.get(&house_name).copied().unwrap_or(0));
 
-    for (key, value) in houses {
+    for (key, value) in &houses {
         println!("{}: {}", key, value);
     }
+
+    houses.insert(String::from("Ravenclaw"), 30);
+
+    println!("{} has {} points", house_name, houses.get(&house_name).copied().unwrap_or(0));
+
+    // Entry method
+    houses.entry(String::from("Papillionlisse")).or_insert(30);
+    houses.entry(String::from("Hufflepuff")).or_insert(30);
+
+    println!("{:?}", houses);
+
+    //Updating a Value Based on the Old Value
+    let text = "It takes a great deal of bravery to stand up to our enemies but just as much to \
+    stand up to our friends";
+
+    let mut word_count = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = word_count.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", word_count);
 
 }
